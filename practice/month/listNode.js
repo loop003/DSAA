ee410a062d53cbb677a10d9eeffe82df5f936058
Mxintro <986695652@qq.com>
2021-03-23 17:08:10 +0800

    ListNode: delete

--- practice/month/listNode.js
+++ practice/month/listNode.js
@@ -1,6 +1,6 @@
-function ListNode (val) {
-  this.val = val
-  this.next = null
+function ListNode (val, next) {
+  this.val = val === undefined ? 0 : val
+  this.next = next === undefined ? null : next
 }
 /**
  *
@@ -9,10 +9,14 @@ const L1 = new ListNode(1)
 let curL1 = L1
 curL1.next = new ListNode(2)
 curL1 = curL1.next
+curL1.next = new ListNode(2)
+curL1 = curL1.next
 curL1.next  = new ListNode(4)
 
 const L2 = new ListNode(1)
 let curL2 = L2
+curL2.next = new ListNode(3)
+curL2 = curL2.next
 curL2.next = new ListNode(3)
 curL2 = curL2.next
 curL2.next  = new ListNode(4)
@@ -44,5 +48,82 @@ function mergeTwoLists (L1, L2) {
   return head.next
 }
 
-const LL = mergeTwoLists(L1, L2)
-console.log(readList(LL))
+// 有序链表去重
+function deleteDuplicates (head) {
+  let cur = head
+  while(cur && cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next
+    } else { 
+      cur = cur.next
+    }
+  } 
+  return head
+}
+
+// 有序链表去重,只保留原始链表中 没有重复出现的数字。
+// 1 3 3 4 8
+function deleteDuplicatesAll (head) {
+  let font = head
+  let cur = font
+  while(cur && cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next
+      while (cur.val === cur.next.val) {
+        cur.next = cur.next.next
+      }
+      cur = cur.next
+      font.next = cur
+    } else {
+      font = cur 
+      cur = cur.next
+    }  
+  }
+  return head
+}
+
+// 给定一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+function deleteN (head, n) {
+  const dummy = new ListNode()
+  dummy.next = head
+  let fast = dummy
+  let slow = dummy
+  let len = 0
+  while(fast.next) {
+    fast = fast.next
+    len++
+    if (len-n > 0) {
+      slow = slow.next
+    }
+  }
+  slow.next = slow.next.next
+  return dummy.next
+}
+
+function deleteN2 (head, n) {
+  const dummy = new ListNode()
+  dummy.next = head
+  let fast = dummy
+  let slow = dummy
+
+  while(n > 0) {
+    fast = fast.next
+    n--
+  }
+  while(fast.next) {
+    fast = fast.next
+    slow = slow.next
+  }
+  slow.next = slow.next.next
+  return dummy.next
+}
+
+const head = new ListNode()
+const head2 = new ListNode()
+head.next = L1
+head2.next = L2
+// console.log(readList(head))
+// console.log(readList(deleteDuplicates(head)))
+console.log(readList(head2))
+// console.log(readList(deleteDuplicatesAll(head2)))
+console.log(readList(deleteN2(head2,1)))
