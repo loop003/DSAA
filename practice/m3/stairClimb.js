a96db884b938d9609997dcd8d4179ab559e568b8
Mxintro <986695652@qq.com>
2021-05-03 22:40:56 +0800

    db: stair climb

--- practice/m3/stairClimb.js
+++ practice/m3/stairClimb.js
@@ -1,7 +1,18 @@
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
 function stairClimb(n) {
   const res = []
-  const
-   function dp() {
-     
-   }
+  res[0] = 0
+  res[1] = 1
+  res[2] = 2
+
+  function dp(n) {
+    if (n>2){
+      res[n] = dp(n-1) + dp(n-2)
+    }
+    return res[n]
+  }
+
+  return dp(n)
 }
+
+console.log(stairClimb(5))
